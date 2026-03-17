b247d896d836245fd3faf37cf14dc1e29f457d32
Boyan G. Kiroff <boyan.kiroff@brandmaker.com>
2026-03-17 11:19:32 +0200

    Initial commit Bootstrap auth-server with Spring Boot OAuth2 Authorization Server # add Maven wrapper and project scaffolding # configure app name and entrypoint # include initial tests # add README with run/test/build notes

---
+++ src/test/java/org/kiroff/auth_server/oauth/AuthServerApplicationTests.java
@@ -0,0 +1,15 @@
+package org.kiroff.auth_server.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
